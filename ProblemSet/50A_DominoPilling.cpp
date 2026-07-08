fcc7578dad9ad4afef9bdd8e3669074ea7691e80
hyunjongsuh <222944122+HenrySHJ@users.noreply.github.com>
2026-07-08 17:21:55 +0900

    Add files via upload

---
+++ ProblemSet/50A_DominoPilling.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+
+using namespace std;
+
+int main() {
+    // Fast IO
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int M, N;
+    cin >> M >> N;
+
+    cout << M * N / 2 << endl;
+}
