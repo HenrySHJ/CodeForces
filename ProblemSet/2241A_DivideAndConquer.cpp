e3d2827eede052406322a3f255165e9e9c45d363
hyunjongsuh <222944122+HenrySHJ@users.noreply.github.com>
2026-07-12 19:33:18 +0900

    Add files via upload

---
+++ ProblemSet/2241A_DivideAndConquer.cpp
@@ -0,0 +1,26 @@
+# include <iostream>
+
+using namespace std;
+
+void solve() {
+    int x, y;
+    cin >> x >> y;
+
+    if (x >= y && x % y == 0)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
+
+int main() {
+    // Fast IO
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+}
