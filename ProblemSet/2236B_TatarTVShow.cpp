7f3b7fe7a624793b5a648aca6d380f28787d9281
hyunjongsuh <222944122+HenrySHJ@users.noreply.github.com>
2026-07-13 14:22:22 +0900

    Add files via upload

---
+++ ProblemSet/2236B_TatarTVShow.cpp
@@ -0,0 +1,43 @@
+# include <iostream>
+# include <string>
+
+using namespace std;
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < n - k; i++) {
+        if (s[i] == '1') {
+            s[i] = '0';
+
+            if (s[i + k] == '1') 
+                s[i + k] = '0';
+            else s[i + k] = '1';
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (s[i] == '1') {
+            cout << "NO" << endl;
+            return;
+        }
+    }
+    cout << "YES" << endl;
+    return;
+}
+int main() {
+    // Fast IO
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+}
